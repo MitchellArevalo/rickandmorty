ed67f962ce1fca619e82063664980dee03df6863
MitchellArevalo <107509488+MitchellArevalo@users.noreply.github.com>
2023-03-16 16:02:13 -0500

    comit con imagenes

--- src/SliderHome/SliderHome.jsx
+++ src/SliderHome/SliderHome.jsx
@@ -2,20 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import './SliderHome.css';
+import img1 from "../Recursos/IMG1.jpg"
+import img2 from "../Recursos/img2.jpg"
+import img3 from "../Recursos/img3.jpg"
 
 const SliderHome = () => {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character')
-      .then(response => response.json())
-      .then(data => setCharacters(data.results));
-  }, []);
+  const slideImages = [
+    {
+      url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+      image: img1
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+      image: img2
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+      image: img3
+    },
+  ];
 
   return (
     <div className="slide-container">
       <Slide>
-       {characters.map((slideImage, index)=> (
+       {slideImages.map((slideImage, index)=> (
           <div key={index}>
             <div className='divStyle' >
               <img className="imgSlider" src={slideImage.image} alt={slideImage.url}/>
